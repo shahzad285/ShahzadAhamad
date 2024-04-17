308b0326f1a840b16cc260bf260eedb7626fd93b
DESKTOP-S59BJJM\personal <shahzadahamad285@gmail.com>
2024-04-17 11:54:03 +0530

    Changes done for Education tab

--- shahzadahamad/src/components/Education.tsx
+++ shahzadahamad/src/components/Education.tsx
@@ -2,11 +2,12 @@ export default function Education()
 {
     return <>
     <p className="bg-teal-700 text-5xl font-thin pl-10 text-white py-8">EDUCATION</p>
-                <div className="bg-white my-16 center ">
+                <div className="bg-white my-24 center flex flex-col justify-center items-center space-y-4">
                     <p className="text-teal-700 font-medium text-xl">UNIVERSITY INSTITUTE OF ENGINEERING AND TECHNOLOGY, PANJAB UNIVERSITY</p>
-                    <p className="text-stone-600">Chandigarh, Punjab</p>
+                    <p className="text-teal-700 font-medium text-xl">2010-2014</p>
+                    <p className="text-stone-600 text-lg">Chandigarh, Punjab</p>
                     <ul>
-                        <li>B.E Information Technology</li>
+                        <li className="text-lg">B.E(Information Technology)</li>
                     </ul>
 
                 </div>
